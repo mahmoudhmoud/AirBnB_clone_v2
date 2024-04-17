d77857c1b4f7502be446aa934f5daebdfc769809
Madani00 <mamchart@gmail.com>
2024-04-17 16:06:12 -0700

    g

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,28 @@ class BaseModel(Base):
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.id = str(uuid.uuid4())
+        if not kwargs:
+            from models import storage
+
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            if kwargs.get("created_at"):
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get("updated_at"):
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            else:
+                self.updated_at = datetime.now()
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -35,9 +55,14 @@ class BaseModel(Base):
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = self.__dict__.copy()
-        dictionary.pop('_sa_instance_state', None)
-        return dictionary
+        dct = self.__dict__.copy()
+        dct['__class__'] = self.__class__.__name__
+        for k in dct:
+            if type(dct[k]) is datetime:
+                dct[k] = dct[k].isoformat()
+        if '_sa_instance_state' in dct.keys():
+            del(dct['_sa_instance_state'])
+        return dct
 
     def delete(self):
         """by calling the method, delete the current instance from the storage"""
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
+from models.amenity import Amenity
 from models.base_model import Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.place import place_amenity
+
+classes = {"User": User, "State": State, "City": City,
+           "Amenity": Amenity, "Place": Place, "Review": Review}
 
 class DBStorage:
     """Private class attributes"""
