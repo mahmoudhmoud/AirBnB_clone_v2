ba1be277637d29aab37c768539cdde09f0d8971c
mahmoudhmoud <jadmclarence@gamil.com>
2024-05-03 18:11:52 -0700

    tetrety

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' laktam man tltsd '''
+
+from datetime import datetime
+from fabric.api import *
+
+
+def do_pack():
+    ''' sawab ikon tam '''
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
+        return None
