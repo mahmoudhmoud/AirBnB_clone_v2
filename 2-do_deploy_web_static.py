a369de686e813528e9198a03d5462a2ee56893c3
mahmoudhmoud <jadmclarence@gamil.com>
2024-05-03 18:19:30 -0700

    tetert

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' sawab ala assas iksda tam '''
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.236.44.236', '107.20.13.19']
+
+
+def do_deploy(archive_path):
+    ''' ldakira dyal dawwe '''
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
