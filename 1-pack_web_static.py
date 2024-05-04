612681ecb033327b3530044a137a55297f0e249c
mahmoudhmoud <jadmclarence@gamil.com>
2024-05-03 18:13:13 -0700

    tetyryt

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
 
     time = datetime.now()
     archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
+    local("mkdir -p versions")
     create = local('tar -cvzf versions/{} web_static'.format(archive))
     if create is not None:
         return archive
